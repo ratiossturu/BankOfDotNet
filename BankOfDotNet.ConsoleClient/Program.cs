f996653600605f6016c55c7fc38c2dd9649ec5f8
rsturua <rsturua@gpih.ge>
2019-12-06 18:05:50 +0400

    Add project files.

---
+++ BankOfDotNet.ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankOfDotNet.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
